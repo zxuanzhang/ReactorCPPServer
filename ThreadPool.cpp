892368975918a3d4471cdbdff65fa2113363dd35
zxuanzhang <zhangzx_2024@163.com>
2024-11-28 15:12:43 +0000

    add threadpool function

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,17 +1,43 @@
 #include "ThreadPool.h"
-
-ThreadPool* ThreadPoolInit(EventLoop* mainloop, int count)
+#include<stdlib.h>
+#include<assert.h>
+struct ThreadPool* ThreadPoolInit(struct EventLoop* mainloop, int count)
 {
-
-	return nullptr;
+	struct ThreadPool* pool = (struct ThreadPool*)malloc(sizeof(struct ThreadPool));
+	pool->index = 0;
+	pool->isStart = false;
+	pool->threadNums = count;
+	pool->mainloop = mainloop;
+	pool->threads = (struct WorkThread*)malloc(sizeof(struct WorkThread) * count);
+	return pool;
 }
 
 void ThreadPoolRun(ThreadPool* pool)
 {
-
+	assert(pool && !pool->isStart);
+	//确定是在主线程运行的
+	if (pool->mainloop->pid != pthread_self()) {
+		exit(0);
+	}
+	pool->isStart = true;
+	if (pool->threadNums) {//threadNums大于零证明有子线程，是多反应堆模型，否则是但反应堆模型
+		for (int i = 0;i < pool->threadNums;++i) {
+			WorkThreadInit(&pool->threads[i], i);
+			WorkThreadRun(&pool->threads[i]);
+		}
+	}
 }
 
-EventLoop* takeEventLoop(ThreadPool* pool)
+struct EventLoop* takeEventLoop(struct ThreadPool* pool)
 {
-	return nullptr;
+	assert(pool->isStart);
+	if (pool->mainloop->pid != pthread_self()) {
+		exit(0);
+	}
+	struct EventLoop* evloop = pool->mainloop;
+	if (pool->threadNums) {
+		evloop = pool->threads[pool->index].evloop;
+		pool->index = ++pool->index % pool->threadNums;
+	}
+	return evloop;
 }
